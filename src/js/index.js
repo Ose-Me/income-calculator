94cda4a0561779b83531906a6371af03132169db
ose-me <oseeronlan@gmail.com>
2020-10-31 00:16:28 +0100

    feat:create income calculator

---
+++ src/js/index.js
@@ -0,0 +1,41 @@
+const income = (data) => {
+  // initialise earnings to 0
+  let total = 0;
+  let overtime = 0;
+  let earnings = 0;
+
+  console.log(data);
+  earnings = data.hours * 1000
+  overtime = data.extraHours * 200;
+  total =earnings + overtime;
+  return { earnings, overtime, total };
+};
+
+// get input data
+function getData(e) {
+  e.preventDefault();
+
+  var hours = parseFloat(document.getElementById("hours").value);
+  var extraHours = parseFloat(document.getElementById("extra-hours").value);
+
+  let data = {
+   hours:hours,
+   extraHours:extraHours,
+    
+  };
+
+  // assign result of income calculation to result variable
+  var result = income(data);
+
+  console.log(result);
+  document.getElementById("earnings").innerHTML = result.earnings.toFixed(
+    2
+  );
+  document.getElementById("overtime").innerHTML = result.overtime.toFixed(2);
+  document.getElementById("total-earnings").innerHTML = result.total.toFixed(2);
+}
+
+// onsubmit of the form call the getData function
+document
+  .getElementById("income-calculator")
+  .addEventListener("submit", getData);
