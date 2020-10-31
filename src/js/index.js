835d30fe3aff8ac3294ec91ed81ee64de71207d6
ose-me <oseeronlan@gmail.com>
2020-10-31 10:27:35 +0100

    fix: calculate earnings for one day

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,16 @@ const income = (data) => {
   let earnings = 0;
 
   console.log(data);
-  earnings = data.hours * 1000
-  overtime = data.extraHours * 200;
-  total =earnings + overtime;
+  if (data.hours <= 4) {
+    earnings = data.hours * 1000;
+    total = earnings;
+  }
+  if (data.hours > 4) {
+    earnings = 4 * 1000;
+    overtime = (data.hours-4) * 200;
+    total = (4 * 1000) + overtime;
+  }
+  
   return { earnings, overtime, total };
 };
 
@@ -16,21 +23,18 @@ function getData(e) {
   e.preventDefault();
 
   var hours = parseFloat(document.getElementById("hours").value);
-  var extraHours = parseFloat(document.getElementById("extra-hours").value);
+  // var extraHours = parseFloat(document.getElementById("extra-hours").value);
 
   let data = {
-   hours:hours,
-   extraHours:extraHours,
-    
+    hours: hours,
+    //  extraHours:extraHours,
   };
 
   // assign result of income calculation to result variable
   var result = income(data);
 
   console.log(result);
-  document.getElementById("earnings").innerHTML = result.earnings.toFixed(
-    2
-  );
+  document.getElementById("earnings").innerHTML = result.earnings.toFixed(2);
   document.getElementById("overtime").innerHTML = result.overtime.toFixed(2);
   document.getElementById("total-earnings").innerHTML = result.total.toFixed(2);
 }
